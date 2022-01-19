e9b49bbefac91815bf38989056518e2cd1cc46b6
Biel333 <khava.sharapova@gmail.com>
2022-01-19 19:42:21 +0300

    +6 автотестов по сравнению, есть все, кроме удалить секцию и удалить несколько товаров

--- helpers/compare_helper.py
+++ helpers/compare_helper.py
@@ -3,18 +3,57 @@ from decouple import config
 
 
 class CompareHelper:
+    # добавить товар в сравнение
     @staticmethod
-    def add_product(productId): #добавить товар в сравнение
+    def add_product(productId):
         return request(
             method='PUT',
             rout=f'/api/v1/compare/{productId}?show_entity=',
-            authorization=True
-        )
+            authorization=True)
 
+    # очистить сравнение
     @staticmethod
-    def delete_compare(): #очистить сравнение
+    def delete_compare():
         return request(
             method='DELETE',
             rout='/api/v1/compare',
-            authorization=True
-        )
+            authorization=True)
+
+    # удалить товар из сравнения (один товар)
+    @staticmethod
+    def delete_product(productId):
+        return request(
+            method='DELETE',
+            rout=f'/api/v1/compare/{productId}?show_entity=',
+            authorization=True)
+
+    # получить структуру сравнения
+    @staticmethod
+    def get_section():
+        return request(
+            method='GET',
+            rout='/api/v1/compare',
+            authorization=True)
+
+    # получить список разделов в сравнении
+    @staticmethod
+    def get_section_list():
+        return request(
+        method='GET',
+        rout='/api/v1/compare/sections',
+        authorization=True)
+
+    @staticmethod
+    def get_all_products():
+        return request(
+        method='GET',
+        rout='/api/v1/compare/products',
+        authorization=True)
+
+    @staticmethod
+    def delete_section(sectionId, iblockId):
+        return request(
+            method='DELETE',
+            rout=f'/api/v1/compare/section/{sectionId}/{iblockId}',
+            authorization=True)
+
--- tests/test_compare.py
+++ tests/test_compare.py
@@ -4,12 +4,40 @@ from helpers.compare_helper import CompareHelper
 
 class TestAddProductCompare:
     def setup_class(self):
-        self.responce_add = CompareHelper.add_product("619051")
         self.responce_delete = CompareHelper.delete_compare()
+        self.responce_add = CompareHelper.add_product("740643")
+        self.responce_delete_product = CompareHelper.delete_product("740643")
+        self.responce_get_section = CompareHelper.get_section()
+        self.responce_get_section_list = CompareHelper.get_section_list()
+        self.responce_get_all_products = CompareHelper.get_all_products()
+        self.responce_delete_section = CompareHelper.delete_section("", "706")
 
-    def test_delete_comare(self):
+    # очистить сравнение
+    def test_delete_compare(self):
         assert self.responce_delete.status_code == 200
 
+    # добавить товар в сравнение
     def test_add_product(self):
         assert self.responce_add.status_code == 200
+
+    # удалить товар из сравнения (один товар)
+    def test_delete_product(self):
+        assert self.responce_delete_product.status_code == 200
+
+    # получить структуру сравнения
+    def test_get_section(self):
+        assert self.responce_get_section.status_code == 200
+
+    # получить список разделов в сравнении
+    def test_get_section_list(self):
+        assert self.responce_get_section_list.status_code == 200
+
+    # получить все товары сравнения
+    def test_get_all_products(self):
+        assert self.responce_get_all_products.status_code == 200
+
+
+    # удалить раздел в сравнении
+    def test_delete_section(self):
+        assert
 
