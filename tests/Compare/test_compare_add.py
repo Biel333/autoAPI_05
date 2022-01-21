9a6aeaaf76e3259d9a9ebb48b24919a4a13761b7
Biel333 <khava.sharapova@gmail.com>
2022-01-21 16:47:11 +0300

    Не помню что за изменения, кажется вывод в консоль значения ответа

--- tests/Compare/test_compare_add.py
+++ tests/Compare/test_compare_add.py
@@ -1,11 +1,18 @@
 from helpers.compare_helper import CompareHelper
+import json
 
 
 class TestCompareAdd:
     def setup_class(self):
-        #ID товаров: 619098, 619045
-        self.responce_add = CompareHelper.add_product("619098")
+        #ID товаров: 619098, 619045, 623277, 601520
+        self.responce_add = CompareHelper.add_product("601520")
+        self.responce_get_all_products = CompareHelper.get_all_products()
 
     # добавить товар в сравнение
     def test_add_product(self):
         assert self.responce_add.status_code == 200
+
+        print(json.dumps(
+            self.responce_get_all_products.json(),
+            indent=5
+        ))
--- tests/Compare/test_compare_get.py
+++ tests/Compare/test_compare_get.py
@@ -1,4 +1,5 @@
 from helpers.compare_helper import CompareHelper
+import json
 
 
 class TestCompareGet:
@@ -10,11 +11,23 @@ class TestCompareGet:
     # получить структуру сравнения
     def test_get_section(self):
         assert self.responce_get_section.status_code == 200
+        print(json.dumps(
+            self.responce_get_section.json(),
+            indent=5
+        ))
 
     # получить список разделов в сравнении
     def test_get_section_list(self):
         assert self.responce_get_section_list.status_code == 200
+        print(json.dumps(
+            self.responce_get_section_list.json(),
+            indent=5
+        ))
 
     # получить все товары сравнения
     def test_get_all_products(self):
         assert self.responce_get_all_products.status_code == 200
+        print(json.dumps(
+            self.responce_get_all_products.json(),
+            indent=5
+        ))
